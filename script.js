5e489b4a92ba1e6693a7c2bf855cd2eb87cf04f8
LWP202 <stagiyev579@gmail.com>
2023-11-22 16:15:48 +0400

    first commit

---
+++ script.js
@@ -0,0 +1,24 @@
+//  let btn = document.querySelector('button')
+//  let body  = document.querySelector('body')
+// btn.addEventListener('click',()=>{
+//        let div = document.createElement('div')
+//        div.style.height ='200px'
+//        div.style.width ='200px'
+//        div.style.backgroundColor='orange'
+//        body.append(div)
+
+// })
+
+
+// let myArray = [{
+//     name:"Sara" ,
+//      age : 20 
+     
+// } ,
+// {
+//     name : " samir" ,
+//     age : 21 
+// }
+ 
+
+// ]
