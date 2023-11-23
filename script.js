ddb3d3884d0969dd4a5811f67350d45a7a9e301d
khaticaa <mirzoevaxedice0@gmail.com>
2023-11-23 16:43:37 +0400

    siniftaskjs9

--- script.js
+++ script.js
@@ -10,15 +10,53 @@
 // })
 
 
+// let ul = document.querySelector("ul")
 // let myArray = [{
-//     name:"Sara" ,
-//      age : 20 
-     
-// } ,
-// {
-//     name : " samir" ,
-//     age : 21 
-// }
- 
-
+//     name:"Sara" , age : 20 
+// } ,{
+//     name:"Sara" , age : 20 
+// } ,{
+//     name:"Sara" , age : 20 
+// } 
 // ]
+
+// for(let i =0 ; i < myArray.length ; i++) {
+//     let li = document.createElement('li')
+//     li.innerText=myArray[i].name + '-' +myArray[i].age
+//     ul.append(li)
+// }
+
+
+// let inputbtn = document.querySelector('#inputbtn')
+// let inputone = document.querySelector('#inputone')
+// let inputtwo = document.querySelector('#inputtwo')
+// inputbtn.addEventListener('click',() =>{
+//     // let div = document.createElement('div')
+//     let div={}
+//     div.inputone=inputone.value
+//     div.inputtwo=inputtwo.value
+//      console.log(div)
+// })
+
+
+// let nav = document.querySelector('nav')
+// let nextElementSibling =nav.firstElementChild.nextElementSibling.firstElementChild;
+// console.log(nextElementSibling);
+// let div = nav.firstElementChild.nextElementSibling.nextElementSibling;
+// console.log(div);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
